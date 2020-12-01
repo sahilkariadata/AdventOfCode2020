28a035e7881a3b8328d7e2dcade0be5efc4d3fd9
Sahil Karia Grover <MacBook_Sahil@Sahils-MacBook-Air.local>
2020-12-01 16:29:06 +0000

    master

---
+++ day1/day1_2.py
@@ -0,0 +1,20 @@
+e_report = open("day1data.txt").read().splitlines()
+#e_report = [1721,979,366,299,675,1456]
+num = len(e_report)
+found = 0
+
+for i in range(num):
+    for j in range(i+1,num):
+        for k in range(j+1,num):
+            if int(e_report[i]) + int(e_report[j]) + int(e_report[k]) == 2020:
+                out = int(e_report[i])*int(e_report[j])*int(e_report[k])
+                found = 1
+                break
+        else:
+            if found == 1:
+                break
+    else:
+        if found == 1:
+            break
+
+print(out)
